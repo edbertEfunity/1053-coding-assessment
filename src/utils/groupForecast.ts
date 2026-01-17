366619c9570adda70de673aaad0138f9f8032f5a
Edbert <edbert.wu@efunity.tech>
2026-01-17 19:43:20 +0800

    Created groupForecast to group the forecast list by day - so each day for 5 days with 3 hour interval will have the correct forecast shown

---
+++ src/utils/groupForecast.ts
@@ -0,0 +1,24 @@
+/* To group the list of forecast info by the correct day */
+import type { forecastInfo, forecastDay } from '@/types/forecastType'
+
+export function groupByDay(forecast: forecastInfo): forecastDay[] | undefined {
+  const days: forecastDay[] = []
+  const today = new Date().toISOString().slice(0, 10)
+
+  for (const f of forecast) {
+    let day = days.find((d) => d.day === f.day)
+
+    if (!day) {
+      day = {
+        day: f.day,
+        title: f.day === today ? 'Today' : undefined,
+        rows: [],
+      }
+
+      days.push(day)
+    }
+
+    day.rows.push(f)
+  }
+  return days
+}
